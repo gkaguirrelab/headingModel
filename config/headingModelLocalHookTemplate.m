106a8e6a7a14e603ef071042f4924ab3d14091f2
gkaguirre <gkaguirre@me.com>
2022-01-18 10:27:29 -0500

    First commit

---
+++ config/headingModelLocalHookTemplate.m
@@ -0,0 +1,31 @@
+function headingModelLocalHook
+%  headingModelLocalHook
+%
+% For use with the ToolboxToolbox.
+%
+% If you 'git clone' ILFContrastAnalsysis into your ToolboxToolbox "projectRoot"
+% folder, then run in MATLAB
+%   tbUseProject('headingModel')
+% ToolboxToolbox will set up IBIOColorDetect and its dependencies on
+% your machine.
+%
+% As part of the setup process, ToolboxToolbox will copy this file to your
+% ToolboxToolbox localToolboxHooks directory (minus the "Template" suffix).
+% The defalt location for this would be
+%   ~/localToolboxHooks/LFContrastAnalsysisLocalHook.m
+%
+% Each time you run tbUseProject('headingModel'), ToolboxToolbox will
+% execute your local copy of this file to do setup for LFContrastAnalsysis.
+%
+% You should edit your local copy with values that are correct for your
+% local machine, for example the output directory location.
+%
+
+
+projectName = 'headingModel';
+
+%% Delete any old prefs
+if (ispref(projectName))
+    rmpref(projectName);
+end
+
