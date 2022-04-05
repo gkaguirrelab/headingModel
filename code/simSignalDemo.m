873a82f3b33775051deacfc730777aba2ba05b4f
gkaguirre <gkaguirre@me.com>
2022-04-05 14:08:00 -0400

    Randomize stimulus order, plot bin output

--- code/simSignalDemo.m
+++ code/simSignalDemo.m
@@ -11,26 +11,18 @@ fileName = fullfile(fileparts(fileparts(mfilename('fullpath'))),'data',[sub '_ci
 % Load the stimulus and data variables
 load(fileName,'stimulus','data')
 
-%% Optional
-% This code replaces the actual stimulus with a simplified version. The
-% heading direction is set to be pi/2 a, in
-% which the heading direction rotates through the available angles over a
-% cycle time of 45 TRs.
-simBins = 45;
-preferredDirection = pi/2;
+
+%% Simulated a heading direction
+% This code replaces the actual stimulus with a fully randomized ordering
+% of heading values, sampled from a specified number of discrete headings.
+nTRs = size(stimulus{1},2); % TRs per acquisition
+simBins = 8;
 binSeparation = (2*pi/simBins);
 binCenters = 0:binSeparation:(2*pi)-binSeparation;
-[~,idx]=min(abs(binCenters - preferredDirection));
-preferredDirection = binCenters(idx);
-nonPreferredDirections = binCenters((1:simBins)~=idx);
-thisVec = nan(1,330);
-idx = logical(zeros(1,330));
-idx(1:simBins:330) = 1;
-thisVec(idx) = preferredDirection;
-thisVec(~idx) = datasample(nonPreferredDirections,sum(~idx));
 for ii=1:length(stimulus)
-    stimulus{ii} = thisVec;
+    stimulus{ii} = datasample(binCenters,nTRs);
 end
+
 
 % The TR of the experiment, in seconds
 tr=2;
@@ -45,17 +37,21 @@ model = heading(data,stimulus,tr,modelOpts{:});
 % Get the x0 param values
 x0 = model.initial;
 
-% We are working in % change units, so set the first (gain) parameter of
-% the adaptation effect to a value in units of % change (something between
-% 0 and 0.5 would be typical)
+% Set the gain of the adaptation effect to zero.
 x0(1) = 0;
 
-% The filter bin parameters are at index positions [5:5+nFilterBins-1]. Set
-% a value for one of the bins
+% Pick a preferred direction, which is the bin center closest to the
+% preferred direction
+preferredDirection = pi/2;
 binSeparation = (2*pi/nFilterBins);
 binCenters = 0:binSeparation:(2*pi)-binSeparation;
+[~,idx]=min(abs(binCenters - preferredDirection));
+preferredDirection = binCenters(idx);
+fprintf('The preferred direction is: %2.2f rads\n',preferredDirection)
+
+% Set the parameter for the preferred direction to unity
 [~,idx] = min(abs(binCenters - preferredDirection));
-myBin = 4+idx;
+myBin = 4+idx; % The first four parameters handle the adaptation model
 x0(myBin) = 1;
 
 % Get the simulated signal for the x params
@@ -63,7 +59,8 @@ simSignal = model.forward(x0);
 
 % Plot this
 figure
-subplot(3,1,1);
+subplot(4,1,1);
+thisVec = stimulus{1};
 plot(thisVec,'-k');
 hold on
 idx = find(thisVec==preferredDirection);
@@ -74,8 +71,8 @@ title(sprintf('simulated heading direction (bins = %d)',simBins));
 set(gca,'YTick',0:pi/2:2*pi) 
 set(gca,'YTickLabel',{'0','pi/2','pi','3*pi/2','2*pi'})
 
-subplot(3,1,2);
-plot(simSignal(1:330));
+subplot(4,1,2);
+plot(simSignal(1:nTRs));
 xlabel('time [TRs]');
 ylabel('BOLD response');
 title(sprintf('simulated BOLD response (bins = %d)',nFilterBins));
@@ -98,22 +95,20 @@ x1 = results.params;
 fitSignal = model.forward(x1);
 
 % Add this to the plot
-subplot(3,1,3);
-plot(fitSignal(1:330));
+subplot(4,1,3);
+plot(fitSignal(1:nTRs));
 xlabel('time [TRs]');
 ylabel('BOLD response');
 title(sprintf('fitted BOLD response (bins = %d)',nFilterBins));
 
 % Compare the simulated and recovered values
-fprintf('Overall gain [simulated vs recovered]: %2.2f vs. %2.2f \n',x0(1),x1(1));
-fprintf('Overall exponent [simulated vs recovered]: %2.2f vs. %2.2f \n',x0(2),x1(2));
-fprintf('Overall time constant [simulated vs recovered]: %2.2f vs. %2.2f \n',x0(4),x1(4));
-fprintf('Heading bins: \n');
-fprintf('  simulated: %2.1f, %2.1f, %2.1f, %2.1f, %2.1f, %2.1f, %2.1f, %2.1f \n',x0(5:5+nFilterBins-1));
-fprintf('  recovered: %2.1f, %2.1f, %2.1f, %2.1f, %2.1f, %2.1f, %2.1f, %2.1f \n',x1(5:5+nFilterBins-1));
-fprintf('HRF params: \n');
-fprintf('  simulated: %2.2f, %2.2f, %2.2f \n',x0(5+nFilterBins:5+nFilterBins+2));
-fprintf('  recovered: %2.2f, %2.2f, %2.2f \n',x1(5+nFilterBins:5+nFilterBins+2));
+subplot(4,1,4);
+plot(binCenters,x0(5:5+nFilterBins-1),'-k');
+hold on
+plot(binCenters,x1(5:5+nFilterBins-1),'*r');
+set(gca,'XTick',0:pi/2:2*pi) 
+set(gca,'XTickLabel',{'0','pi/2','pi','3*pi/2','2*pi'})
+ylabel('model parameter');
+xlabel('bin centers [rads]');
+title('simulated and recovered bin amplitude');
 
-% Could try adding some noise to simSignal and see how well you recover the
-% parameters
