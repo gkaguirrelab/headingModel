9fff77d686a9bb70210a9abd540c5769e887000b
gkaguirre <gkaguirre@me.com>
2022-06-14 11:31:20 -0400

    Implemented von Misses distribution
    This lead us to discover a serious bug: The model weights were highly dependent upon sigma in the old version. This is no longer the case.

--- code/@heading/forward.m
+++ code/@heading/forward.m
@@ -36,16 +36,18 @@ adaptGain = x(1);   % Gain of the adaptation effect
 epsilon = x(2);     % Non-linear exponent of the neural signal
 %tau = x(3);         % Time constant of the temporal integration
 
+
 %% Build a model of absolute heading direction
 % To start we will model a single preferred heading directon with a
-% circular gaussian of width sigma, and orientation of alpha, and amplitude
-% gamma.
+% circular gaussian of width sigma (converted into the von Misses
+% concentration parameter kappa).
 binSeparation = (2*pi/nFilterBins);
 sigmaVal = binSeparation;
+kappa = sqrt(1/sigmaVal^2);
 neuralSignal = zeros(size(stimulus));
 binCenters = 0:binSeparation:(2*pi)-binSeparation;
 for ii = 1:nFilterBins
-    thisFilterResponse = x(nFixedParams+ii) .* normpdf(angdiff(stimulus, repmat(binCenters(ii),size(stimulus))./sigmaVal));
+    thisFilterResponse = x(nFixedParams+ii) .* circ_vmpdf(stimulus,binCenters(ii),kappa);
     neuralSignal = neuralSignal + thisFilterResponse;
 end
 
@@ -152,57 +154,4 @@ hrf = flobsbasis*x';
 % Normalize the kernel to have unit area
 hrf = hrf/sum(abs(hrf));
 
-end
-
-function [p alpha] = circ_vmpdf(alpha, thetahat, kappa)
-% [p alpha] = circ_vmpdf(alpha, w, p)
-%   Computes the circular von Mises pdf with preferred direction thetahat 
-%   and concentration kappa at each of the angles in alpha
-%
-%   The vmpdf is given by f(phi) =
-%   (1/(2pi*I0(kappa))*exp(kappa*cos(phi-thetahat)
-%
-%   Input:
-%     alpha     angles to evaluate pdf at, if empty alphas are chosen to
-%               100 uniformly spaced points around the circle
-%     [thetahat preferred direction, default is 0]
-%     [kappa    concentration parameter, default is 1]
-%
-%   Output:
-%     p         von Mises pdf evaluated at alpha
-%     alpha     angles at which pdf was evaluated
-%
-%
-%   References:
-%     Statistical analysis of circular data, Fisher
-%
-% Circular Statistics Toolbox for Matlab
-
-% By Philipp Berens and Marc J. Velasco, 2009
-% velasco@ccs.fau.edu
-
-% Modified by MNau - Spring, 2019
-
-% if no angles are supplied, 100 evenly spaced points around the circle are
-% chosen
-if nargin < 1 || isempty(alpha)
-    alpha = linspace(0, 2*pi, 101)';
-    alpha = alpha(1:end-1);
-end
-if nargin < 3
-    kappa = 1;
-end
-if nargin < 2
-    thetahat = 0;
-end
-
-alpha = alpha(:);
-
-% Original:
-% evaluate pdf
-% C = 1/(2*pi*besseli(0,kappa));
-% p = C * exp(kappa*cos(alpha-thetahat));
-
-% New:
-p = exp(kappa*(cos(alpha-thetahat)-1)) / (2*pi*besseli(0,kappa,1));
 end
